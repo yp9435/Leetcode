a057927839602688eb5b4e47f3dbb42b0a8953cd
Yeshaswi <147942578+yp9435@users.noreply.github.com>
2025-07-23 13:12:23 +0530

    Time: 3 ms (64.01%), Space: 19.2 MB (17.21%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,6 +1,25 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        stack = [] 
+        stack = []
+        # PUSH A NUMBER AND whne u come across a +-/* pop 2 elements from the stack and do the operation put the answer back in stack
+        for token in tokens:
+            if token in '+-/*':
+                n1, n2 = stack.pop(), stack.pop()
+                if token == "+":
+                    new = n2 + n1
+                elif token == "-":
+                    new = n2 - n1
+                elif token == "/":
+                    new = int(n2 / n1)
+                elif token == "*":
+                    new = n2 * n1
+                stack.append(new)
+            else:
+                stack.append(int(token))
+        return stack[-1]
+    
+        
+        '''stack = [] 
         for c in tokens:
             if c == "+":  
                 stack.append(stack.pop() + stack.pop())
@@ -14,4 +33,4 @@ class Solution:
                 stack.append(int(float(b) / a))  # Perform division and truncate towards zero
             else:
                 stack.append(int(c))  # If the token is a number, convert to int and push onto the stack
-        return stack[0] 
+        return stack[0]'''
